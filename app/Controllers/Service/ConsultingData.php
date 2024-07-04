ac9e3254c69f7a95b9619f161a578e898efaa1c3
ezaratec <ezarate@tracklink.pe>
2024-07-04 10:07:47 -0500

    Se borraron líneas innecesarias

--- app/Controllers/Service/ConsultingData.php
+++ app/Controllers/Service/ConsultingData.php
@@ -54,41 +54,20 @@ class ConsultingData extends ResourceController
 
         // $data = $this->execute_sp($var1, $var2, $var3, $var4, $var5, $var6, $var7, $var8, $var9, $var10, $var11);
 
-        // $data = $this->db->query("exec sp_Rep_AlmacenSalidas_MTC
-        // @Empresa  = '$var1',  
-        // @Localidad ='$var2',
-        // @Almacen ='$var3',     
-        // @transaccion ='$var4',
-        //   @Entidad ='$var5',
-        //   @OPSerie ='$var6',    
-        //   @OPNumero ='$var7',    
-        //   @Linea ='$var8',       
-        //   @Producto ='$var9',    
-        //   @FechaI='$var10',      
-        //   @FechaF='$var11'");
+        $data = $this->db->query("exec sp_Rep_AlmacenSalidas_MTC
+        @Empresa  = '$var1',  
+        @Localidad ='$var2',
+        @Almacen ='$var3',     
+        @transaccion ='$var4',
+          @Entidad ='$var5',
+          @OPSerie ='$var6',    
+          @OPNumero ='$var7',    
+          @Linea ='$var8',       
+          @Producto ='$var9',    
+          @FechaI='$var10',      
+          @FechaF='$var11'");
 
-        $data = $this->db->query("SELECT C.Anexo,A.Descripcion As AnexoDescripcion,A.Direccion,C.OP_Numero_Doc, IH.imeiExtrae, IH.marca , IH.modelo ,IH.codHomologacion
-        FROM TransDet D Inner Join TransCab      C On D.Id_Transaccion=C.Id_Transaccion
-                         Left Join Transacciones T On C.Empresa=T.Empresa And C.TipoTransaccion=T.TipoTransaccion And C.Transaccion=T.Transaccion
-                         Left Join Anexos        A On C.Id_Anexo=A.Id_Anexo
-                         Left Join Productos     P On D.Empresa=P.Empresa And D.Producto=P.Producto
-                         Left Join Almacen       U On D.Empresa=U.Empresa And C.Localidad=U.Localidad and D.Almacen=U.Almacen
-                         Left Join Lineas        L On P.Empresa=L.Empresa And P.Linea=L.Linea
-                         INNER JOIN InfoImeiHomologacion IH On SUBSTRING(C.OP_Numero_Doc, 1,4) = IH.imeiExtrae
-       WHERE D.Empresa='20525107915' 
-         And C.TipoTransaccion='2'
-         And D.TipoRegistro='M'
-         And C.Serie_Doc in ('001','003')
-         And Case When '01'=''   Then '01'   else C.Localidad end='01'
-         And Case When '001'=''     Then '001'     else D.Almacen end='001'
-         And Case When ''='' Then '' else C.Transaccion end=''
-         And Case When ''=''     Then ''     else C.Anexo end=''
-         And Case When ''=''     Then ''     else C.OP_Serie_Doc end=''
-         And Case When ''=''    Then ''     else C.OP_Numero_Doc end=''
-         And Case When '001'=''       Then '001'       else P.Linea end='001'
-         And Case When ''=''    Then ''    else D.Producto end=''
-         And Fecha Between 45413-2 And 45443-2
-         AND (A.Direccion is NULL or A.Direccion = '')");
+
 
         $respuestaBSOFT = $data->getResultArray();
 
